b3d2ecf54a3041fde47dc2a2d4c1969760c72416
Aditya <adityachakma199@gmail.com>
2020-04-16 12:19:22 +0600

    ExexuteInEditMode. Grid added. 125

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField][Range(1f,20f)] private float gridSize;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize ) *gridSize;
+        snapPos.y = 0;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize ) *gridSize;
+        transform.position = snapPos;
+
+        //transform.localScale = new Vector3(gridSize,gridSize,gridSize);
+    }
+
+}
