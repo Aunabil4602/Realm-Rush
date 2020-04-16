b4d982649adbaa524535e501ab0d056654f1f020
Aditya <adityachakma199@gmail.com>
2020-04-16 13:17:21 +0600

    Grids added. Text mesh added. Selection Base added. 126

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class EditorSnap : MonoBehaviour
 {
-    [SerializeField][Range(1f,20f)] private float gridSize;
+    [SerializeField][Range(1f,20f)] private float gridSize = 10f ;
+    [SerializeField] private TextMesh gridPosition;
+    [SerializeField][Range(1f,20f)] private float uniforScaling = 10f;
 
     void Update()
     {
@@ -14,8 +16,10 @@ public class EditorSnap : MonoBehaviour
         snapPos.y = 0;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize ) *gridSize;
         transform.position = snapPos;
+        
+        gridPosition.text = transform.position.z/10 + "," + transform.position.x/10;
 
-        //transform.localScale = new Vector3(gridSize,gridSize,gridSize);
+        transform.localScale = new Vector3(uniforScaling,uniforScaling,uniforScaling);
     }
 
 }
